ee1305cc9718d1a145d55d50cb8eb1559fca7288
Daris02 <103503966+Daris02@users.noreply.github.com>
2022-07-11 10:27:38 +0300

    Update Book.java

--- Java Project/DevoirProg2/app/src/main/java/com/exo/Book.java
+++ Java Project/DevoirProg2/app/src/main/java/com/exo/Book.java
@@ -17,21 +17,19 @@ public class Book
   public int getQuantity(){ return this.quantity ; }
   public void setQuantity(int Quantity){ this.quantity = Quantity ; }
   
-  Author authorB;
-  public Author getAuthorB(){ return this.authorB ; }
-  public void setAuthorB(Author AuthorB){ this.authorB = AuthorB ; }
+  Author author;
   
   //Constructor : 
-  public Book(String titre, double prix, int quantity, Author authorB){
+  public Book(String titre, double prix, int quantity, Author author){
     this.titre = titre;
     this.prix = prix;
     this.quantity = quantity;
-    this.authorB = authorB;
+    this.author = author;
   }
   
   //Method : 
   public String toString(){
-    return "Book : "+titre+" a un prix de "+prix+" et à pour author "+authorB+". ";
+    return "Book : "+titre+" a un prix de "+prix+" et à pour author "+author+". ";
   }
 }
 
